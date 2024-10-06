2747f110141e08bb9edc8e050a3a493faec52f53
AishwaryaNagchandi <aishwaryanagchandi84@gmail.com>
2024-10-06 20:07:17 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/ui/button'
+import { Outlet } from 'react-router-dom'
 
 function App() {
+  const [count, setCount]=useState()
+  const {user,isLoaded,isSignedIn}=useUser();
+  if(!isSignedIn) 
+    {
+      return <Navigate to={'/auth/sign-in'}/>
+    }
 
+  
   return (
     <>
+    <Outlet/>
     </>
   )
 }
